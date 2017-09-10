96cf495079b2d5ab3856fc78d649c843fcf10e8f
Caleb Riggs <criggs2892@flpoly.org>
2017-09-10 19:07:30 -0400

    Updated front end for interaction.

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,10 @@ to update the database.
 Created By: Caleb Riggs
 */
 
+const DEBUG = true;
+var fs = require('fs');
 //establish the frontEnd director structure
+
 const ROOT_DIR = "../frontEnd/";
 module.exports = function (app, passport, express, MongoClient,url,mongo,md5) {
     //pull additional dependency
@@ -17,6 +20,23 @@ module.exports = function (app, passport, express, MongoClient,url,mongo,md5) {
 
     app.use('/home', isLoggedIn,function(req, res) {
       send(res, "home.html");
+    });
+
+    app.post('/updatePicture',upload.single('file'), isLoggedIn,function(req, res) {
+      var validIn="header1,header2,header3,header4,header5,group,ist,logo";
+      if(validIn.includes(req.body.picID)){
+        fs.rename(req.file.path, "../frontEnd/img/test.jpg", function (err) {
+          if (err){
+            console.error(err);
+            res.send("error: issue uploading file");
+          }
+          res.send('Success: New Image Uploaded');
+        });
+      }
+      else{
+        console.error("Invalid Input");
+        res.send("error: invalid input");
+      }
     });
 
     app.get('/getMembers',function(req, res) {
@@ -54,7 +74,7 @@ module.exports = function (app, passport, express, MongoClient,url,mongo,md5) {
     function isLoggedIn(req, res, next) {
       // return next();
       // if user is authenticated in the session, carry on
-      if (req.isAuthenticated())
+      if (req.isAuthenticated() || DEBUG)
           return next();
 
       // if they aren't redirect them to the home page
