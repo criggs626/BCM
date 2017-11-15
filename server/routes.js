785f8cdf9bb6330cd797de61ddba946e47de20a4
Caleb Riggs <criggs@ip-172-31-8-199.us-east-2.compute.internal>
2017-11-15 03:00:03 +0000

    updated images and routes

--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,7 @@ Created By: Caleb Riggs
 const DEBUG = false;
 var fs = require('fs');
 var multer=require('multer');
-var globVar={groups:0,students:0,date:"",active:false,questions:[]};
+var globVar={groups:0,students:0,date:"10/4/2017",active:false,questions:[]};
 //establish the frontEnd director structure
 
 const ROOT_DIR = "../frontEnd/";
